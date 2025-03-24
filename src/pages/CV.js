099210062a8da8cb5187a94a51f8612ccefe564e
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2025-03-24 22:05:04 +0000

    Add cv preview and pdf download links from google docs.

--- src/pages/CV.js
+++ src/pages/CV.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Col, Row } from "react-bootstrap";
 
 const CV = () => {
-  return (
-    <div>
-      <h1>CV</h1>
-    </div>
-  )
-}
+    return (
+        <Row className="d-flex justify-content-center vh-100">
+            <Col md={10} lg={8} className="d-flex flex-column">
+                <Row className="my-3">
+                    <Col className="text-center">
+                        <h1>Curriculum Vitae</h1>
+                    </Col>
+                </Row>
+                <Row className="mb-4">
+                    <Col className="text-center">
+                        <a
+                            href="https://docs.google.com/document/d/17uJhLz-DtSs8fvVJofUdbCmJobCH03VodBppBGQtNxY/export?format=pdf"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="btn btn-primary">
+                            Download My CV (PDF)
+                        </a>
+                    </Col>
+                </Row>
+                <Row className="flex-grow-1">
+                    <Col>
+                        <iframe
+                            title="My CV"
+                            src="https://docs.google.com/document/d/17uJhLz-DtSs8fvVJofUdbCmJobCH03VodBppBGQtNxY/preview"
+                            width="100%"
+                            height="90%"
+                            style={{ border: "none" }}></iframe>
+                    </Col>
+                </Row>
+                
+            </Col>
+        </Row>
+    );
+};
 
-export default CV
+export default CV;
