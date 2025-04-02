f56347bb8d58a5f4e25ee1f01412fdb60a225815
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2025-04-02 18:37:37 +0100

    Add bee teach to projects and adjust projects page styling.

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -5,15 +5,15 @@ import Styles from "../styles/ProjectDetail.module.css";
 
 const ProjectDetail = ({ src, name, description, link }) => {
     return (
-        <Row className="d-flex justify-content-center vh-100 my-4">
+        <Row className="d-flex justify-content-center my-4">
             <Col md={10} lg={8} className="d-flex flex-column">
                 <Row>
-                    <a href="{link}" target="_blank" className={Styles.Name}>
+                    <a href={link} target="_blank" rel="noreferrer" className={Styles.Name}>
                         {name}
                     </a>
                 </Row>
                 <Row>
-                    <Col className="text-center">
+                    <Col className="text-center mb-5">
                         <Image src={src} alt="Project preview" fluid />
                         <p>{description}</p>
                     </Col>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,17 +3,18 @@ import { Col, Row } from "react-bootstrap";
 import ProjectDetail from "../components/ProjectDetail";
 
 import freeflow from "../assets/freeflow.png";
+import beeteach from "../assets/beeteach.gif";
 
 const Projects = () => {
     return (
-        <Row className="d-flex justify-content-center vh-100">
+        <Row className="d-flex justify-content-center">
             <Col md={10} lg={8} className="d-flex flex-column">
                 <Row className="my-4">
                     <Col className="text-center">
-                        <h1>Projects</h1>
+                        <h1 className="m-0">Projects</h1>
                     </Col>
                 </Row>
-                <Row className="my-4">
+                <Row>
                     <Col className="text-center">
                         <ProjectDetail
                             src={freeflow}
@@ -23,6 +24,16 @@ const Projects = () => {
                             While developing the app I followed an agile methodology and I made use of project boards, 
                             carried out full testing and wrote in depth documentation for the entire process, all of which can be found in the repo."
                             link="https://github.com/KyleMardell/freeflow-app"
+                        />
+
+                        <ProjectDetail
+                            src={beeteach}
+                            name="Bee Teach - Educational Resource Sharing App"
+                            description="Bee Teach is an educational resource sharing app designed for teachers, parents and educators alike. 
+                            The app was the first full stack project in the Code Institute - Full Stack Software Development Diploma, 
+                            and was created using Django for the entire front and back ends, as well as a Postgresql database to store data.
+                            The repo contains detailed documentation of the entire development process including extensive manual and unit testing."
+                            link="https://github.com/KyleMardell/bee-teach"
                         />
                     </Col>
                 </Row>
